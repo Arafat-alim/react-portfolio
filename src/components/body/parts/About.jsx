a3b81e75e453fb15a58e7798b6b943aa08b24d8a
Arafat <arafat97alim@gmail.com>
2021-11-23 02:59:55 +0530

    about me part is processing.

--- src/components/body/parts/About.jsx
+++ src/components/body/parts/About.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 const About = () => {
-  return <div>THis is About</div>;
+  return (
+    <div className="about">
+      <div className="about-top">
+        <div className="about-info">
+          Hello There 👋 I am
+          <br />
+          <span className="info-name">Arafat Alim!</span>
+          <br />I love experimenting with the web
+        </div>
+        <div className="about-photo">
+          <img
+            src={require("../../../assets/coding-bro.png").default}
+            alt="coding-boy"
+            className="coding-boy"
+          />
+        </div>
+      </div>
+      <div className="about-bottom"></div>
+    </div>
+  );
 };
 
 export default About;
