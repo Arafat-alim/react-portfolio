19ed0f64f89f5e67b9cab15b96edaa8824d755e6
Arafat <arafat97alim@gmail.com>
2021-11-22 21:28:13 +0530

    added home component. Arrangin components.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import React from "react";
-import "./App.css";
-import Body from "./components/Body";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
+
+import Home from "./components/Home";
 
 function App() {
   return (
     <div>
-      <div>
-        <Header />
-      </div>
-      <div>
-        <Body />
-      </div>
-      <div>
-        <Footer />
-      </div>
+      <Home />
     </div>
   );
 }
