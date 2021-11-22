0197744a353b0d5f2981af5c3826419c30e49213
Arafat <arafat97alim@gmail.com>
2021-11-22 19:10:03 +0530

    component based architecture. Components added.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
 import "./App.css";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <div>
+        <Header />
+      </div>
+      <div>
+        <Body />
+      </div>
+      <div>
+        <Footer />
+      </div>
     </div>
   );
 }
