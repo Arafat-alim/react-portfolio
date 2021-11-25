cdc396795ca773ebe61b171d678215f6996d832a
Arafat <arafat97alim@gmail.com>
2021-11-26 02:05:03 +0530

    contact component is on process.

--- src/components/body/parts/Contact.jsx
+++ src/components/body/parts/Contact.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import SocialContact from "../../common/SocialContact";
 
 const Contact = () => {
-  return <div>THis is Contact</div>;
+  return (
+    <div className="contact">
+      <div className="section-title">Contacts</div>
+      <div className="contact-left">
+        <div className="contact-container">
+          <p>Want to get Touch? Contact me on these Platform</p>
+          <SocialContact />
+        </div>
+        <div className="download">
+          <a href={require("../../../assets/Resume.pdf").default} download>
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/724/724933.png"
+              alt="download-logo"
+              className="resume-logo"
+            />
+            Download Resume
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
