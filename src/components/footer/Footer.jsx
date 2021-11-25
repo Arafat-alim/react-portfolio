2d973378712fd69f66bb975828c973589d51cd62
Arafat <arafat97alim@gmail.com>
2021-11-26 02:30:20 +0530

    footer component is completed with styling.

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import "./footer.css";
 
 function Footer() {
-  return <div>Footer</div>;
+  return <div className="footer">Made with ❤️ by NeckReact</div>;
 }
 
 export default Footer;
