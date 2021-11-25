ef3f5db727bcf203241d33f5c2081b342e3b8794
Arafat <arafat97alim@gmail.com>
2021-11-26 00:38:20 +0530

    updated project card images resolution and tags.

--- src/data/projects.js
+++ src/data/projects.js
@@ -16,46 +16,46 @@ export const projects = [
     demoLink: "https://arafat-crypto-app.netlify.app/",
     githubLink: "https://github.com/Arafat-alim/crypto-app",
     image:
-      "https://res.cloudinary.com/cocoder/image/upload/v1637684451/Cryptoverse_kz3u7u.png",
+      "https://res.cloudinary.com/cocoder/image/upload/v1637866322/Cryptoverse_ykj1yw.png",
   },
   {
     id: 2,
     title: "Frontend - Cloud Hosting Site",
-    desc: "",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa tempora illum commodi eveniet, autem quae ipsa nulla ea totam eius, sit possimus quas! Voluptatem delectus deleniti recusandae architecto nesciunt vero?",
     tags: ["HTML", "Javascript", "CSS"],
     demoLink: "https://arafat-alim.ml",
     githubLink: "https://github.com/Arafat-alim/Moshify",
     image:
-      "https://res.cloudinary.com/cocoder/image/upload/v1637684689/Moshify_aw90va.png",
+      "https://res.cloudinary.com/cocoder/image/upload/v1637866326/Moshify_mzwz7u.png",
   },
   {
     id: 3,
     title: "Project 3",
-    desc: "Lorem Ip",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa tempora illum commodi eveniet, autem quae ipsa nulla ea totam eius, sit possimus quas! Voluptatem delectus deleniti recusandae architecto nesciunt vero?",
     tags: ["ReactJs", "NodeJs"],
     demoLink: "https://arafat-crypto-app.netlify.app/",
     githubLink: "https://github.com/Arafat-alim/crypto-app",
     image:
-      "https://res.cloudinary.com/cocoder/image/upload/v1637684451/Cryptoverse_kz3u7u.png",
+      "https://res.cloudinary.com/cocoder/image/upload/v1637866322/Cryptoverse_ykj1yw.png",
   },
   {
     id: 4,
     title: "Project 4",
-    desc: "Lorem Ip",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa tempora illum commodi eveniet, autem quae ipsa nulla ea totam eius, sit possimus quas! Voluptatem delectus deleniti recusandae architecto nesciunt vero?",
     tags: ["ReactJs", "NodeJs"],
     demoLink: "https://arafat-crypto-app.netlify.app/",
     githubLink: "https://github.com/Arafat-alim/crypto-app",
     image:
-      "https://res.cloudinary.com/cocoder/image/upload/v1637684451/Cryptoverse_kz3u7u.png",
+      "https://res.cloudinary.com/cocoder/image/upload/v1637866326/Moshify_mzwz7u.png",
   },
   {
     id: 5,
     title: "Project 1",
-    desc: "Lorem Ip",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa tempora illum commodi eveniet, autem quae ipsa nulla ea totam eius, sit possimus quas! Voluptatem delectus deleniti recusandae architecto nesciunt vero?",
     tags: ["ReactJs", "NodeJs"],
     demoLink: "https://arafat-crypto-app.netlify.app/",
     githubLink: "https://github.com/Arafat-alim/crypto-app",
     image:
-      "https://res.cloudinary.com/cocoder/image/upload/v1637684451/Cryptoverse_kz3u7u.png",
+      "https://res.cloudinary.com/cocoder/image/upload/v1637866322/Cryptoverse_ykj1yw.png",
   },
 ];
