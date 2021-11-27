83b6862c4ae2768c23e5bc7e7f1aa0aebda13a47
Arafat <arafat97alim@gmail.com>
2021-11-28 01:44:54 +0530

    works data updated

--- src/data/works.js
+++ src/data/works.js
@@ -3,28 +3,39 @@ export const workData = [
     company: "Newton's School",
     designation: "Full Stack MERN",
     dateJoining: "01.01.2021",
-    dateEnd: "Present",
+    dateEnd: "15.11.2021",
     companyLogo:
       "https://res.cloudinary.com/cocoder/image/upload/v1637960496/icon_180x180-b7bd51db7de8a010a275d90024d91ef9_ozwncx.png",
-    workDes:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam blanditiis dolorum magni harum repellat, ex veritatis! Saepe corporis numquam beatae?",
+    workDes: `• Full Stack Web Development along with Problem-Solving. 
+     • Technical Stack Learned: HTML, CSS, Javascript, Bootstrap, ReactJS, Redux, NodeJS, Firebase, Express, MongoDB, MySQL. \n
+    • Worked on various Projects like Drawing-App, Github Profile Search, Covid19-Tracker, CryptoVerse . \n
+    • Participated in a couple of Hackathons organized by Newton School (2021).`,
   },
+  // {
+  //   company: "Google",
+  //   designation: "Frontend",
+  //   dateJoining: "01.01.2022",
+  //   dateEnd: "Present",
+  //   companyLogo: "https://cdn-icons-png.flaticon.com/512/2702/2702602.png",
+  //   workDes:
+  //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam blanditiis dolorum magni harum repellat, ex veritatis! Saepe corporis numquam beatae?",
+  // },
+  // {
+  //   company: "Facebook",
+  //   designation: "Full Stack MERN",
+  //   dateJoining: "01.01.2023",
+  //   dateEnd: "Present",
+  //   companyLogo: "https://cdn-icons-png.flaticon.com/512/733/733547.png",
+  //   workDes:
+  //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam blanditiis dolorum magni harum repellat, ex veritatis! Saepe corporis numquam beatae?",
+  // },
   {
-    company: "Google",
-    designation: "Frontend",
-    dateJoining: "01.01.2022",
+    company: "Genpact",
+    designation: "Software Developer Engineer JAVA (SDE)",
+    dateJoining: "31.11.2021",
     dateEnd: "Present",
-    companyLogo: "https://cdn-icons-png.flaticon.com/512/2702/2702602.png",
-    workDes:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam blanditiis dolorum magni harum repellat, ex veritatis! Saepe corporis numquam beatae?",
-  },
-  {
-    company: "Facebook",
-    designation: "Full Stack MERN",
-    dateJoining: "01.01.2023",
-    dateEnd: "Present",
-    companyLogo: "https://cdn-icons-png.flaticon.com/512/733/733547.png",
-    workDes:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam blanditiis dolorum magni harum repellat, ex veritatis! Saepe corporis numquam beatae?",
+    companyLogo:
+      "https://res.cloudinary.com/cocoder/image/upload/v1638043230/Genpact_Logo_b2ns8l.jpg",
+    workDes: "I am here a dot net Developer",
   },
 ];
