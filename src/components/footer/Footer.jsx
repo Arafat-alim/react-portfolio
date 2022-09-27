46fd499c8e7d9a98441a5939ae43158e62187134
arafat-alim <arafat97alim@gmail.com>
2022-09-27 22:59:59 +0530

    feature Update - profile visit added

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./footer.css";
+import countapi from "countapi-js";
 
 function Footer() {
-  return <div className="footer">Made with ❤️ Arafat Alim &copy;</div>;
+  const [count, setCount] = React.useState(0);
+  // ! Visiting page
+  useEffect(() => {
+    countapi.visits("arafat-alim2.com").then((result) => {
+      // console.log(result.value);
+      setCount(result.value);
+    });
+  }, []);
+  // // ! gettng data from the api
+  // useEffect(() => {
+  //   countapi
+  //     .get("arafat-alim2.com", "3cfc5e4b-0e60-40ce-bcc1-f826df3c51d3")
+  //     .then((result) => {
+  //       console.log(result.value);
+  //       setCount(result.value);
+  //     });
+  // }, [count]);
+  return (
+    <div className="footer">
+      <span className="viewing">
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/2874/2874802.png"
+          alt="views"
+        />
+        <p id="visits">{count}</p>
+      </span>
+      <div>Made with ❤️ Arafat Alim &copy;</div>
+    </div>
+  );
 }
 
 export default Footer;
